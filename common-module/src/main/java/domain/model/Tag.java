6cf5c42a0fb6b21f721c00d599e2283d9488f4e6
Alex Won <dnjscksdn98@gmail.com>
2020-09-25 22:42:07 +0900

    refactor: cleanup tag domain model

--- common-module/src/main/java/domain/model/Tag.java
+++ common-module/src/main/java/domain/model/Tag.java
@@ -1,8 +1,9 @@
 package domain.model;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import domain.status.UserTag;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,9 +13,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import org.hibernate.annotations.CreationTimestamp;
-import domain.status.UserTag;
-import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Tag {
@@ -26,12 +27,14 @@ public class Tag {
   @Enumerated(value = EnumType.STRING)
   private UserTag name;
 
-  @Column(nullable = false, updatable = false)
-  @CreationTimestamp
-  private LocalDateTime createdAt;
-  @UpdateTimestamp
-  private LocalDateTime updatedAt;
-
-  @OneToMany(fetch = FetchType.LAZY, mappedBy = "tag")
+  @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY)
   private List<Board> boards = new ArrayList<>();
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
 }
