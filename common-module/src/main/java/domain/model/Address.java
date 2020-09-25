2d8b192ef4479c0558370ad8896ce751d7669af8
Alex Won <dnjscksdn98@gmail.com>
2020-09-25 22:40:45 +0900

    refactor: cleanup address domain model

--- common-module/src/main/java/domain/model/Address.java
+++ common-module/src/main/java/domain/model/Address.java
@@ -1,8 +1,9 @@
 package domain.model;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import domain.status.CityType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,9 +13,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import org.hibernate.annotations.CreationTimestamp;
-import domain.status.CityType;
-import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Address {
@@ -23,15 +24,17 @@ public class Address {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @OneToMany(fetch = FetchType.LAZY, mappedBy = "address")
-  private List<User> user = new ArrayList<>();;
-
   @Enumerated(value = EnumType.STRING)
   private CityType city;
 
-  @Column(nullable = false, updatable = false)
+  @OneToMany(mappedBy = "address", fetch = FetchType.LAZY)
+  private List<User> users = new ArrayList<>();
+
   @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
   private LocalDateTime createdAt;
+
   @UpdateTimestamp
+  @Column(name = "updated_at")
   private LocalDateTime updatedAt;
 }
