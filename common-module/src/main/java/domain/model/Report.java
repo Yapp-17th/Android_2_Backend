970162ed8bbb9dd3b17fe5c529d8bc74d13a7584
Alex Won <dnjscksdn98@gmail.com>
2020-09-26 10:53:28 +0900

    refactor: cleanup report domain model

--- common-module/src/main/java/domain/model/Report.java
+++ common-module/src/main/java/domain/model/Report.java
@@ -1,5 +1,9 @@
 package domain.model;
 
+import domain.status.ReportType;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -8,7 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import domain.status.ReportType;
+import java.time.LocalDateTime;
 
 @Entity
 public class Report {
@@ -25,5 +29,11 @@ public class Report {
 
   @Enumerated(value = EnumType.ORDINAL)
   private ReportType type;
+
+  @Column(nullable = false)
   private String content;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
 }
