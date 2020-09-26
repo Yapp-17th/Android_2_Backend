ae866872110902fa1afa50539bd62cf511960c36
Alex Won <dnjscksdn98@gmail.com>
2020-09-26 11:40:43 +0900

    refactor: cleanup chatroom domain model

--- common-module/src/main/java/domain/model/ChatRoom.java
+++ common-module/src/main/java/domain/model/ChatRoom.java
@@ -1,17 +1,22 @@
 package domain.model;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import domain.status.ChatRoomStatus;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class ChatRoom {
@@ -20,15 +25,25 @@ public class ChatRoom {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Enumerated(value = EnumType.ORDINAL)
+  private ChatRoomStatus status = ChatRoomStatus.ACTIVE;
+
   @ManyToOne(fetch = FetchType.LAZY)
   private User host;
 
   @ManyToOne(fetch = FetchType.LAZY)
   private User guest;
 
-  @Column(nullable = false, updatable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Board board;
+
   @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
   private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
 
   @OneToMany(mappedBy = "chatRoom", fetch = FetchType.LAZY)
   private List<Message> messages = new ArrayList<>();
