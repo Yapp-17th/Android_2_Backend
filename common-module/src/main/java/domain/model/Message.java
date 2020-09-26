e7d864b21f4fe5e51233cd96d44f653482fef71a
Alex Won <dnjscksdn98@gmail.com>
2020-09-26 11:17:11 +0900

    refactor: cleanup message domain model

--- common-module/src/main/java/domain/model/Message.java
+++ common-module/src/main/java/domain/model/Message.java
@@ -1,16 +1,18 @@
 package domain.model;
 
-import java.time.LocalDateTime;
+import domain.status.MessageType;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
-import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
 
 @Entity
 public class Message {
@@ -19,17 +21,23 @@ public class Message {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false)
+  private String content;
+
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.ORDINAL)
+  private MessageType type;
+
+  @Column(name = "is_read", nullable = false)
+  private boolean isRead = false;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private User sender;
+
   @ManyToOne(fetch = FetchType.LAZY)
   private ChatRoom chatRoom;
-  @NotNull
-  private Integer userId;
 
-  private String contents;
-  private Integer type;
-
-  @Column(nullable = false, updatable = false)
   @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
   private LocalDateTime createdAt;
-
-  private Integer isRead = 0;
 }
