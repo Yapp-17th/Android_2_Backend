d0d855b485c3e032bfabddb40a949411d5ae7974
Alex Won <dnjscksdn98@gmail.com>
2020-09-28 02:29:21 +0900

    feat: Add builder, getter, and setter functions to entities (#6)
    * @getter, @Setter 어노테이션 추가
    * Builder 패턴 구현
    * 기본 생성자를 @NoArgsConstructor 로 대체
    * 필요한 제약사항들은 추가 및 수정
    
    * TODO: add, delete, increase, decrease, update

--- common-module/src/main/java/com/yapp/crew/domain/model/Alarm.java
+++ common-module/src/main/java/com/yapp/crew/domain/model/Alarm.java
@@ -1,7 +1,12 @@
 package com.yapp.crew.domain.model;
 
 import com.yapp.crew.domain.status.AlarmType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -10,18 +15,46 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Alarm extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Setter(value = AccessLevel.PRIVATE)
+  @Column(nullable = false)
   @Enumerated(value = EnumType.ORDINAL)
   private AlarmType type;
 
+  @Setter(value = AccessLevel.PRIVATE)
+  @Column(nullable = false)
   private String content;
 
-  protected Alarm() {
+  public static AlarmBuilder getBuilder() {
+    return new AlarmBuilder();
+  }
 
+  public static class AlarmBuilder {
+    private AlarmType type;
+    private String content;
+
+    public AlarmBuilder withType(AlarmType type) {
+      this.type = type;
+      return this;
+    }
+
+    public AlarmBuilder withContent(String content) {
+      this.content = content;
+      return this;
+    }
+
+    public Alarm build() {
+      Alarm alarm = new Alarm();
+      alarm.setType(type);
+      alarm.setContent(content);
+      return alarm;
+    }
   }
 }
