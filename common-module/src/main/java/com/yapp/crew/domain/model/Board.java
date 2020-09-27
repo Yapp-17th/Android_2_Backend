130030e291f7e466039aeef03a62555fcf9f0c0a
Alex Won <dnjscksdn98@gmail.com>
2020-09-27 20:58:07 +0900

    chore: add board builder

--- common-module/src/main/java/com/yapp/crew/domain/model/Board.java
+++ common-module/src/main/java/com/yapp/crew/domain/model/Board.java
@@ -1,6 +1,9 @@
 package com.yapp.crew.domain.model;
 
 import com.yapp.crew.domain.status.GroupStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -20,31 +23,101 @@ public class Board extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @Column(nullable = false)
   private String title;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @ManyToOne(fetch = FetchType.LAZY)
   private User user;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @ManyToOne(fetch = FetchType.LAZY)
   private Category category;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @ManyToOne(fetch = FetchType.LAZY)
   private Address address;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @ManyToOne(fetch = FetchType.LAZY)
   private Tag tag;
 
+  @Getter
   @Enumerated(value = EnumType.ORDINAL)
   private GroupStatus status = GroupStatus.RECRUITING;
 
+  @Getter
   @Column(name = "recruit_count", nullable = false)
   private Integer recruitCount = 0;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @Column(name = "starts_at", nullable = false)
   private LocalDateTime startsAt;
 
   protected Board() {
 
+  }
+
+  // TODO: add, delete, increase, decrease, update function
+
+  public static BoardBuilder getBuilder() {
+    return new BoardBuilder();
+  }
+
+  public static class BoardBuilder {
+    private String title;
+    private User user;
+    private Category category;
+    private Address address;
+    private Tag tag;
+    private LocalDateTime startsAt;
+
+    public BoardBuilder withTitle(String title) {
+      this.title = title;
+      return this;
+    }
+
+    public BoardBuilder withUser(User user) {
+      this.user = user;
+      return this;
+    }
+
+    public BoardBuilder withCategory(Category category) {
+      this.category = category;
+      return this;
+    }
+
+    public BoardBuilder withAddress(Address address) {
+      this.address = address;
+      return this;
+    }
+
+    public BoardBuilder withTag(Tag tag) {
+      this.tag = tag;
+      return this;
+    }
+
+    public BoardBuilder withStartsAt(LocalDateTime startsAt) {
+      this.startsAt = startsAt;
+      return this;
+    }
+
+    public Board build() {
+      Board board = new Board();
+      board.setTitle(title);
+      board.setUser(user);
+      board.setCategory(category);
+      board.setAddress(address);
+      board.setTag(tag);
+      board.setStartsAt(startsAt);
+      return board;
+    }
   }
 }
