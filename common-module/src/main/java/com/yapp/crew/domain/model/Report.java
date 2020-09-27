225259e96589bad3f8955cb62cf4629a3fa09a5a
Alex Won <dnjscksdn98@gmail.com>
2020-09-27 21:22:28 +0900

    chore: add report builder & getter & setter

--- common-module/src/main/java/com/yapp/crew/domain/model/Report.java
+++ common-module/src/main/java/com/yapp/crew/domain/model/Report.java
@@ -1,6 +1,9 @@
 package com.yapp.crew.domain.model;
 
 import com.yapp.crew.domain.status.ReportType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -15,21 +18,70 @@ import javax.persistence.ManyToOne;
 public class Report extends BaseEntity {
 
   @Id
+  @Getter
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @ManyToOne(fetch = FetchType.LAZY)
   private User reporter;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @ManyToOne(fetch = FetchType.LAZY)
   private User reported;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   @Enumerated(value = EnumType.ORDINAL)
   private ReportType type;
 
+  @Getter
+  @Setter(value = AccessLevel.PRIVATE)
   private String content;
 
   protected Report() {
 
+  }
+
+  public ReportBuilder getBuilder() {
+    return new ReportBuilder();
+  }
+
+  public static class ReportBuilder {
+    private User reporter;
+    private User reported;
+    private ReportType type;
+    private String content;
+
+    public ReportBuilder withReporter(User reporter) {
+      this.reporter = reporter;
+      return this;
+    }
+
+    public ReportBuilder withReported(User reported) {
+      this.reported = reported;
+      return this;
+    }
+
+    public ReportBuilder withType(ReportType type) {
+      this.type = type;
+      return this;
+    }
+
+    public ReportBuilder withContent(String content) {
+      this.content = content;
+      return this;
+    }
+
+    public Report build() {
+      Report report = new Report();
+      report.setReporter(reporter);
+      report.setReported(reported);
+      report.setType(type);
+      report.setContent(content);
+      return report;
+    }
   }
 }
