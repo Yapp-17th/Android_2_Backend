1b86caee228e58910ac3b7bc6859f3c45b80cdbc
soyeon <sypark9646@gmail.com>
2020-10-15 01:38:05 +0900

    fix: remove repository annotation, fix typo

--- common-module/src/main/java/com/yapp/crew/domain/repository/MessageRepository.java
+++ common-module/src/main/java/com/yapp/crew/domain/repository/MessageRepository.java
@@ -3,9 +3,7 @@ package com.yapp.crew.domain.repository;
 import com.yapp.crew.domain.model.Message;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
   List<Message> findAll();
