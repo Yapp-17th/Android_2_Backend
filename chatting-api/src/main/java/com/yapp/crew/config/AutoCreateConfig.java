a12b725225f7cb114e07d95dd30c430234070c2d
Alex Won <dnjscksdn98@gmail.com>
2020-10-17 17:01:11 +0900

    chore: setup chatting service

---
+++ chatting-api/src/main/java/com/yapp/crew/config/AutoCreateConfig.java
@@ -0,0 +1,21 @@
+package com.yapp.crew.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@Profile(value = "dev")
+public class AutoCreateConfig {
+
+  @Bean
+  public NewTopic exPlanetChat_dev() {
+    return TopicBuilder
+            .name("explanet-dev")
+            .partitions(1)
+            .replicas(1)
+            .build();
+  }
+}
