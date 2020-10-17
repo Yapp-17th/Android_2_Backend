5f05e93d2d478bc5f4571fbcadfcf92dfbf9aa88
Alex Won <dnjscksdn98@gmail.com>
2020-10-17 15:26:22 +0900

    fix: resolve conflicts

---
+++ chatting-consumer/src/main/java/com/yapp/crew/ChattingConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.yapp.crew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChattingConsumerApplication.class, args);
+  }
+}
