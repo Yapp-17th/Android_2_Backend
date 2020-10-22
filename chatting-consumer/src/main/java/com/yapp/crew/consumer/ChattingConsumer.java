4a1a0a14264700810be21a5b1e6b791b4e10877d
Alex Won <dnjscksdn98@gmail.com>
2020-10-22 22:33:32 +0900

    chore: cleanup code

--- chatting-consumer/src/main/java/com/yapp/crew/consumer/ChattingConsumer.java
+++ chatting-consumer/src/main/java/com/yapp/crew/consumer/ChattingConsumer.java
@@ -62,7 +62,7 @@ public class ChattingConsumer {
 
     chattingConsumerService.processMessage(message);
 
-		simpMessagingTemplate.convertAndSend("/sub/chat/room/" + message.getChatRoom().getId().toString(), message);
+		simpMessagingTemplate.convertAndSend("/chat/room/" + message.getChatRoom().getId().toString(), message);
 
 		String messageJson = objectMapper.writeValueAsString(message);
     log.info("Successfully consumed message: {}", messageJson);
