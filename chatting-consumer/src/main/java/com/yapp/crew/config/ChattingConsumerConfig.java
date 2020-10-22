9226f456aa29e9d02295da43c0d609eb718874ad
Alex Won <dnjscksdn98@gmail.com>
2020-10-22 23:14:42 +0900

    fix: resolve conflicts

--- chatting-consumer/src/main/java/com/yapp/crew/config/ChattingConsumerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.yapp.crew.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-
-@Configuration
-@EnableKafka
-public class ChattingConsumerConfig {
-}
