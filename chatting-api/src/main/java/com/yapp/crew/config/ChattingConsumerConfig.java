ee9a5f6a3ed3c2f0c426599362692edde865eea3
Alex Won <dnjscksdn98@gmail.com>
2020-10-22 23:04:07 +0900

    refactor: combine chatting service

---
+++ chatting-api/src/main/java/com/yapp/crew/config/ChattingConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.yapp.crew.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class ChattingConsumerConfig {
+}
