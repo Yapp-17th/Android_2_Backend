e0b322220fbe34641997ebca81a0ef7077a5a335
Alex Won <dnjscksdn98@gmail.com>
2020-10-24 18:18:29 +0900

    chore: create bot message json

---
+++ chatting-service/src/main/java/com/yapp/crew/json/BotMessages.java
@@ -0,0 +1,24 @@
+package com.yapp.crew.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@PropertySource(value = "classpath:bot-messages.json")
+@ConfigurationProperties
+public class BotMessages {
+
+	@JsonProperty(value = "welcome_message")
+	private String welcomeMessage;
+}
