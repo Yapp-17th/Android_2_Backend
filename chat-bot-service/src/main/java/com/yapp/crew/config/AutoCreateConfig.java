4bddba7d9999a1ae3dbef49464c5284fb0f369f6
Alex Won <dnjscksdn98@gmail.com>
2020-10-24 15:52:19 +0900

    chore: config chat bot service

---
+++ chat-bot-service/src/main/java/com/yapp/crew/config/AutoCreateConfig.java
@@ -0,0 +1,22 @@
+package com.yapp.crew.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@Profile(value = "dev")
+public class AutoCreateConfig {
+
+	@Bean
+	public NewTopic exPlanetBot_dev() {
+		return TopicBuilder
+						.name("explanet-bot-dev")
+						.partitions(1)
+						.replicas(1)
+						.build();
+	}
+}
