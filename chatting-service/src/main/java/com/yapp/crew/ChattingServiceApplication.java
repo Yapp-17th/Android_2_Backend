23d8a752e603138143e10aea79d4d5abe03c0556
Alex Won <dnjscksdn98@gmail.com>
2020-10-24 11:42:20 +0900

    refactor: rename module name (#14)

--- chatting-service/src/main/java/com/yapp/crew/ChattingServiceApplication.java
+++ chatting-service/src/main/java/com/yapp/crew/ChattingServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChattingApiApplication {
+public class ChattingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChattingApiApplication.class, args);
+        SpringApplication.run(ChattingServiceApplication.class, args);
     }
 }
