bce3083af621799362b0acfb8f21d2cd528f8ee0
Alex Won <dnjscksdn98@gmail.com>
2020-10-24 13:04:31 +0900

    chore: create response payload

---
+++ chatting-service/src/main/java/com/yapp/crew/payload/ChatRoomResponsePayload.java
@@ -0,0 +1,33 @@
+package com.yapp.crew.payload;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.yapp.crew.domain.status.ChatRoomStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomResponsePayload {
+
+	private Long id;
+
+	private Long hostId;
+
+	private Long guestId;
+
+	private Long boardId;
+
+	private ChatRoomStatus status;
+
+	private LocalDateTime createdAt;
+
+	private List<MessageResponsePayload> messages;
+}
