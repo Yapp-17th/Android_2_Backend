f5a5adb401aa526835e0493698480f8a24e4e828
soyeon <sypark9646@gmail.com>
2020-11-02 01:27:48 +0900

    feat: user/board report type 추가

--- base-api/src/main/java/com/yapp/crew/utils/EnumToList.java
+++ base-api/src/main/java/com/yapp/crew/utils/EnumToList.java
@@ -2,7 +2,9 @@ package com.yapp.crew.utils;
 
 import com.yapp.crew.domain.type.CityType;
 import com.yapp.crew.domain.type.ExerciseType;
+import com.yapp.crew.domain.type.ReportType;
 import com.yapp.crew.domain.type.UserTag;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,5 +21,27 @@ public class EnumToList {
 
   public static List<String> userTypeEnumToList() {
     return Arrays.stream(UserTag.values()).map(UserTag::getName).collect(Collectors.toList());
+  }
+
+  public static List<String> boardReportTypeEnumToList() {
+    ArrayList<String> boardReportTypeList = new ArrayList<>();
+    for (ReportType reportType : ReportType.values()) {
+      if (reportType.toString().startsWith("BOARD")) {
+        boardReportTypeList.add(reportType.getName());
+      }
+    }
+    boardReportTypeList.add(ReportType.OTHERS.getName());
+    return boardReportTypeList;
+  }
+
+  public static List<String> userReportTypeEnumToList() {
+    ArrayList<String> userReportTypeList = new ArrayList<>();
+    for (ReportType reportType : ReportType.values()) {
+      if (reportType.toString().startsWith("USER")) {
+        userReportTypeList.add(reportType.getName());
+      }
+    }
+    userReportTypeList.add(ReportType.OTHERS.getName());
+    return userReportTypeList;
   }
 }
