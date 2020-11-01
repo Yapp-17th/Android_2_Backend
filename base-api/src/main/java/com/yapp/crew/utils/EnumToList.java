aa270c8f72f4db22b43c28d965af1bf53640630d
soyeon <sypark9646@gmail.com>
2020-11-02 01:11:28 +0900

    feat: user tag controller 추가

--- base-api/src/main/java/com/yapp/crew/utils/EnumToList.java
+++ base-api/src/main/java/com/yapp/crew/utils/EnumToList.java
@@ -2,6 +2,7 @@ package com.yapp.crew.utils;
 
 import com.yapp.crew.domain.type.CityType;
 import com.yapp.crew.domain.type.ExerciseType;
+import com.yapp.crew.domain.type.UserTag;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,5 +15,9 @@ public class EnumToList {
 
   public static List<String> exerciseEnumToList() {
     return Arrays.stream(ExerciseType.values()).map(ExerciseType::getName).collect(Collectors.toList());
+  }
+
+  public static List<String> userTypeEnumToList() {
+    return Arrays.stream(UserTag.values()).map(UserTag::getName).collect(Collectors.toList());
   }
 }
