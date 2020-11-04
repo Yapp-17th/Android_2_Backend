baa67cacd3cdac733f0339094a89e3447c793f69
soyeon <sypark9646@gmail.com>
2020-11-04 16:20:59 +0900

    fix: args error

--- board-api/src/main/java/com/yapp/crew/controller/SearchController.java
+++ board-api/src/main/java/com/yapp/crew/controller/SearchController.java
@@ -37,8 +37,8 @@ public class SearchController {
 
   @GetMapping(path = "/v1/board/search", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<?> getBoardList(@RequestHeader(value = "Authorization") String token, @PageableDefault(size = 20, page = 0) Pageable pageable, @RequestBody @Valid BoardSearchDto boardSearchDto) {
-
-    List<BoardResponseInfo> boardResponseInfoList = searchService.searchBoardList(BoardSearch.build(boardSearchDto));
+    Long userId = auth.parseUserIdFromToken(token);
+    List<BoardResponseInfo> boardResponseInfoList = searchService.searchBoardList(BoardSearch.build(boardSearchDto, userId));
 
     PagedListHolder<BoardResponseInfo> page = new PagedListHolder<>(boardResponseInfoList);
     page.setPageSize(pageable.getPageSize());
