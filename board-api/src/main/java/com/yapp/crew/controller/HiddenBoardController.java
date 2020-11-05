f5944b950b12303129e760b91e06e228cb307ee7
soyeon <sypark9646@gmail.com>
2020-11-05 16:19:53 +0900

    refactor: add cross origin annotation

--- board-api/src/main/java/com/yapp/crew/controller/HiddenBoardController.java
+++ board-api/src/main/java/com/yapp/crew/controller/HiddenBoardController.java
@@ -5,14 +5,18 @@ import com.yapp.crew.dto.request.BoardIdRequestDto;
 import com.yapp.crew.model.SimpleResponse;
 import com.yapp.crew.service.HiddenBoardService;
 import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@CrossOrigin
 @RestController
 public class HiddenBoardController {
 
