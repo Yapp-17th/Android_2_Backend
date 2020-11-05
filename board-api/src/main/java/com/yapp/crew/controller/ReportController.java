b692891844d67d34462d6d6c60f5870528b87175
soyeon <sypark9646@gmail.com>
2020-11-05 16:19:53 +0900

    refactor: add cross origin annotation

--- board-api/src/main/java/com/yapp/crew/controller/ReportController.java
+++ board-api/src/main/java/com/yapp/crew/controller/ReportController.java
@@ -6,14 +6,18 @@ import com.yapp.crew.model.BoardReport;
 import com.yapp.crew.model.SimpleResponse;
 import com.yapp.crew.service.ReportService;
 import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@CrossOrigin
 @RestController
 public class ReportController {
 
