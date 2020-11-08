74492f4e37aabc4086d604d8df75d857169840a3
Alex Won <dnjscksdn98@gmail.com>
2020-11-08 13:37:58 +0900

    chore: setup watcher service

---
+++ watcher-service/src/main/java/com/yapp/crew/WatcherServiceApplication.java
@@ -0,0 +1,19 @@
+package com.yapp.crew;
+
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatcherServiceApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatcherServiceApplication.class, args);
+	}
+}
