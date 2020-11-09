366a99befb65edbd084cae459384de5d7111326f
soyeon <sypark9646@gmail.com>
2020-11-09 18:49:55 +0900

    feat: history 조회

--- mypage-api/src/main/java/com/yapp/crew/model/HistoryListInfo.java
+++ mypage-api/src/main/java/com/yapp/crew/model/HistoryListInfo.java
@@ -2,6 +2,7 @@ package com.yapp.crew.model;
 
 import com.yapp.crew.domain.model.Board;
 import com.yapp.crew.domain.model.User;
+import com.yapp.crew.domain.status.GroupStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,9 +16,11 @@ public class HistoryListInfo {
 	private String leftTime;
 	private BoardListInfo boardInfo;
 
-	public static HistoryListInfo build(Board board, User user){
+	public static HistoryListInfo build(Board board, User user) {
 		HistoryListInfo historyListInfo = new HistoryListInfo();
+		historyListInfo.isHost = board.getUser().getId().equals(user.getId());
 		historyListInfo.userName = user.getNickname();
+		historyListInfo.isContinue = board.getGroupStatus() != GroupStatus.RECRUITING;
 		historyListInfo.leftTime = ""; // TODO: rebase 한 뒤에 추가
 		historyListInfo.boardInfo = BoardListInfo.build(board);
 
