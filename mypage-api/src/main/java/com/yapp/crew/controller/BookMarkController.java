aaf627d47e83102743d06ea7e5fe0a02725963d3
soyeon <sypark9646@gmail.com>
2020-11-09 19:08:09 +0900

    feat: bookmark 조회 추가

--- mypage-api/src/main/java/com/yapp/crew/controller/BookMarkController.java
+++ mypage-api/src/main/java/com/yapp/crew/controller/BookMarkController.java
@@ -1,16 +1,39 @@
 package com.yapp.crew.controller;
 
+import com.yapp.crew.domain.auth.Auth;
+import com.yapp.crew.dto.response.BookMarkListResponseDto;
+import com.yapp.crew.dto.response.UserProfileResponseDto;
+import com.yapp.crew.model.BoardListInfo;
+import com.yapp.crew.model.UserProfileInfo;
 import com.yapp.crew.service.BookMarkService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookMarkController {
 
 	private BookMarkService bookMarkService;
+	private Auth auth;
 
 	@Autowired
-	public BookMarkController(BookMarkService bookMarkService){
+	public BookMarkController(BookMarkService bookMarkService, Auth auth) {
 		this.bookMarkService = bookMarkService;
+		this.auth = auth;
+	}
+
+	@GetMapping(path = "/v1/user/my-profile/bookmark", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> getMyBookmark(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String token) {
+		long userId = auth.parseUserIdFromToken(token);
+
+		List<BoardListInfo> boardListInfos = bookMarkService.getBookMarks(userId);
+		BookMarkListResponseDto bookMarkListResponseDto = BookMarkListResponseDto.build(boardListInfos);
+
+		return ResponseEntity.ok().body(bookMarkListResponseDto);
 	}
 }
--- mypage-api/src/main/java/com/yapp/crew/service/BookMarkService.java
+++ mypage-api/src/main/java/com/yapp/crew/service/BookMarkService.java
@@ -1,10 +1,45 @@
 package com.yapp.crew.service;
 
+import com.yapp.crew.domain.errors.UserNotFoundException;
+import com.yapp.crew.domain.model.BookMark;
+import com.yapp.crew.domain.model.User;
+import com.yapp.crew.domain.repository.BookMarkRepository;
+import com.yapp.crew.domain.repository.UserRepository;
+import com.yapp.crew.model.BoardListInfo;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 public class BookMarkService {
 
+	private UserRepository userRepository;
+	private BookMarkRepository bookMarkRepository;
+
+	@Autowired
+	public BookMarkService(UserRepository userRepository, BookMarkRepository bookMarkRepository) {
+		this.userRepository = userRepository;
+		this.bookMarkRepository = bookMarkRepository;
+	}
+
+	@Transactional
+	public List<BoardListInfo> getBookMarks(long userId) {
+		User user = findUserById(userId)
+				.orElseThrow(() -> new UserNotFoundException("user not found"));
+
+		Set<BookMark> bookMarkSet = user.getUserBookmark();
+		return bookMarkSet.stream()
+				.map(bookMark -> BoardListInfo.build(bookMark.getBoard(), user))
+				.collect(Collectors.toList());
+	}
+
+	private Optional<User> findUserById(Long userId) {
+		return userRepository.findUserById(userId);
+	}
 }
