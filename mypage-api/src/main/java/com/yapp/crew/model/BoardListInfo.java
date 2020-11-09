33d3cc6c4cd7a14fbf1154716ff36f715dcc219c
soyeon <sypark9646@gmail.com>
2020-11-09 19:08:09 +0900

    feat: bookmark 조회 추가

--- mypage-api/src/main/java/com/yapp/crew/model/BoardListInfo.java
+++ mypage-api/src/main/java/com/yapp/crew/model/BoardListInfo.java
@@ -1,6 +1,7 @@
 package com.yapp.crew.model;
 
 import com.yapp.crew.domain.model.Board;
+import com.yapp.crew.domain.model.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,22 +10,29 @@ import lombok.NoArgsConstructor;
 public class BoardListInfo {
 
 	private Long boardId;
+	private Long hostId;
+	private String hostName;
 	private String title;
 	private String groupStatus;
 	private String exercise;
 	private String city;
+	private boolean isBookMar = true;
 	private int recruitNumber;
 	private int recruitedNumber;
+	private String time;
 
-	public static BoardListInfo build(Board board) {
+	public static BoardListInfo build(Board board, User user) {
 		BoardListInfo boardListInfo = new BoardListInfo();
 		boardListInfo.boardId = board.getId();
+		boardListInfo.hostId = board.getUser().getId();
+		boardListInfo.hostName = board.getUser().getNickname();
 		boardListInfo.title = board.getTitle();
 		boardListInfo.groupStatus = board.getGroupStatus().getName(); // TODO: 나중에 바꾸기
 		boardListInfo.exercise = board.getCategory().getExercise().getName();
 		boardListInfo.city = board.getAddress().getCity().getName();
 		boardListInfo.recruitNumber = board.getRecruitCount();
 		boardListInfo.recruitedNumber = board.getApprovedCount();
+		boardListInfo.time = ""; // TODO: rebase 후 업데이트
 
 		return boardListInfo;
 	}
