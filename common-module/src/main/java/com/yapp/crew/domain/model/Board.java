c272d927c612a3f2be1ae191db9ff4d43b07fafb
soyeon <sypark9646@gmail.com>
2020-11-09 14:40:34 +0900

    fix: board 시간 보여주는 부분 추가

--- common-module/src/main/java/com/yapp/crew/domain/model/Board.java
+++ common-module/src/main/java/com/yapp/crew/domain/model/Board.java
@@ -5,8 +5,11 @@ import com.yapp.crew.domain.status.AppliedStatus;
 import com.yapp.crew.domain.status.BoardStatus;
 import com.yapp.crew.domain.status.GroupStatus;
 import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -22,6 +25,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.time.DateUtils;
 
 @Entity
 @Getter
@@ -129,6 +133,25 @@ public class Board extends BaseEntity {
 			return GroupStatus.RECRUITING;
 		}
 		return GroupStatus.COMPLETE;
+	}
+
+	public String showBoardTimeComparedToNow() {
+		Calendar calendar = Calendar.getInstance();
+		Date now = java.sql.Timestamp.valueOf(LocalDateTime.now());
+		Date startDate = java.sql.Timestamp.valueOf(startsAt);
+
+		// TODO: 나중에 GroupStatus == 취소 아니고, 활동 완료 아닐 때로 조건 변경
+		if (status == BoardStatus.NORMAL) {
+			return "[D-"+calculateLeftDays(now, startDate)+"]";
+		}
+
+		calendar.setTime(startDate);
+		return "["+calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DATE)+"]";
+	}
+
+	private int calculateLeftDays(Date now, Date end){
+		long diffInMillies = Math.abs(now.getTime() - end.getTime());
+		return (int)TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
 	}
 
 	private int getApprovedCount() {
