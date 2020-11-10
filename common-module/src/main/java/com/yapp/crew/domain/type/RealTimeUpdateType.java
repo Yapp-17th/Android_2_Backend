7c0b625f776904d492ff564b0129ba7142d74e3b
Alex Won <dnjscksdn98@gmail.com>
2020-11-10 16:03:39 +0900

    chore: add real time update type

---
+++ common-module/src/main/java/com/yapp/crew/domain/type/RealTimeUpdateType.java
@@ -0,0 +1,20 @@
+package com.yapp.crew.domain.type;
+
+import lombok.Getter;
+
+@Getter
+public enum RealTimeUpdateType {
+
+	APPLIED(0, "게시글 신청"),
+	APPROVED(1, "사용자 승인"),
+	DISAPPROVED(2, "사용자 거절"),
+	MESSAGE_READ(3, "메시지 읽음");
+
+	private final int code;
+	private final String name;
+
+	RealTimeUpdateType(int code, String name) {
+		this.code = code;
+		this.name = name;
+	}
+}
