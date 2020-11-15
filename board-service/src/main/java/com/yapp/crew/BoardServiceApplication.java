d8752593600640dc1366652896ad9c9ea8598a88
Alex Won <dnjscksdn98@gmail.com>
2020-11-15 19:47:30 +0900

    fix: rename board service

--- board-service/src/main/java/com/yapp/crew/BoardServiceApplication.java
+++ board-service/src/main/java/com/yapp/crew/BoardServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardApiApplication {
+public class BoardServiceApplication {
 
 	@PostConstruct
 	public void init() {
@@ -14,6 +14,6 @@ public class BoardApiApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoardApiApplication.class, args);
+		SpringApplication.run(BoardServiceApplication.class, args);
 	}
 }
