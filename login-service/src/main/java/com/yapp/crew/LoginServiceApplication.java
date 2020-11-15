edea261cf31327dd1ff788122e92972cd8e35f40
Alex Won <dnjscksdn98@gmail.com>
2020-11-15 19:57:11 +0900

    [hotfix] rename module names (#67)
    * fix: rename mypage service
    
    * fix: rename login service
    
    * fix: rename board service
    
    * fix: rename base service
    
    * fix: rename to service
    
    * fix: comment up test code

---
+++ login-service/src/main/java/com/yapp/crew/LoginServiceApplication.java
@@ -0,0 +1,19 @@
+package com.yapp.crew;
+
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginServiceApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginServiceApplication.class, args);
+	}
+}
