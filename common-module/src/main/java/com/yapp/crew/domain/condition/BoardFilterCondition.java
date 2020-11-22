c73fa01d32caf1298141428c1e9900149c1cc743
soyeon <sypark9646@gmail.com>
2020-11-22 15:17:32 +0900

    feat: querydsl 사용하여 board filter&search

---
+++ common-module/src/main/java/com/yapp/crew/domain/condition/BoardFilterCondition.java
@@ -0,0 +1,27 @@
+package com.yapp.crew.domain.condition;
+
+import com.yapp.crew.domain.type.SortingType;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BoardFilterCondition {
+
+	private long userId;
+	private SortingType sorting = SortingType.LATEST;
+	List<Long> category;
+	List<Long> city;
+
+	public static BoardFilterCondition build(String sorting, List<Long> category, List<Long> city, long userId) {
+		BoardFilterCondition boardFilter = new BoardFilterCondition();
+		boardFilter.userId = userId;
+		boardFilter.sorting = SortingType.getSortingType(sorting);
+		boardFilter.category = category;
+		boardFilter.city = city;
+
+		return boardFilter;
+	}
+}
