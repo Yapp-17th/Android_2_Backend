c62a6d3852c3b2476079c9d115e52ea6d9b9239d
soyeon Park <sypark9646@gmail.com>
2020-11-23 12:41:30 +0900

    feat: initialize insert evaluation list after board status canceled or finished

--- watcher-service/src/main/java/com/yapp/crew/service/BoardBatchService.java
+++ watcher-service/src/main/java/com/yapp/crew/service/BoardBatchService.java
@@ -1,8 +1,13 @@
 package com.yapp.crew.service;
 
 import com.yapp.crew.domain.model.Board;
+import com.yapp.crew.domain.model.Evaluation;
+import com.yapp.crew.domain.model.Evaluation.EvaluationBuilder;
 import com.yapp.crew.domain.repository.BoardRepository;
+import com.yapp.crew.domain.repository.EvaluationRepository;
+import com.yapp.crew.domain.status.AppliedStatus;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,17 +20,46 @@ import org.springframework.stereotype.Service;
 @Service
 public class BoardBatchService {
 
-	private BoardRepository boardRepository;
+	private EvaluationRepository evaluationRepository;
 	private EntityManager entityManager;
 
 	@Autowired
-	public BoardBatchService(BoardRepository boardRepository, EntityManager entityManager) {
-		this.boardRepository = boardRepository;
+	public BoardBatchService(EvaluationRepository evaluationRepository, EntityManager entityManager) {
+		this.evaluationRepository = evaluationRepository;
 		this.entityManager = entityManager;
 	}
 
 	void updateBoardFinishedAll(List<Board> boardList) {
+		saveAll(boardList);
 		boardList.forEach(Board::finishBoard);
 		entityManager.flush();
+	}
+
+	void saveAll(List<Board> boardList){
+		EvaluationBuilder evaluationBuilder = Evaluation.getBuilder();
+
+		for(Board board:boardList){
+			List<Long> userIds =board.getAppliedUsers().stream()
+					.filter(appliedUser -> appliedUser.getStatus() == AppliedStatus.APPROVED)
+					.map(appliedUser -> appliedUser.getUser().getId())
+					.collect(Collectors.toList());
+
+			for(int i=0;i<userIds.size();i++){
+				for(int j=0;j<userIds.size();j++){
+					if(i==j){
+						continue;
+					}
+
+					Evaluation evaluation = evaluationBuilder
+							.withBoard(board)
+							.withEvaluateId(userIds.get(i))
+							.withEvaluateId(userIds.get(j))
+							.withIsDislike(false)
+							.withIsLike(false)
+							.build();
+					evaluationRepository.save(evaluation);
+				}
+			}
+		}
 	}
 }
