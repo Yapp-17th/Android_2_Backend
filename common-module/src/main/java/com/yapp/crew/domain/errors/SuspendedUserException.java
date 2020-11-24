507f63ac427320c5a9591467d3829b68dd634a08
Alex Won <dnjscksdn98@gmail.com>
2020-11-24 16:17:01 +0900

    refactor login service exceptions

--- common-module/src/main/java/com/yapp/crew/domain/errors/SuspendedUserException.java
+++ common-module/src/main/java/com/yapp/crew/domain/errors/SuspendedUserException.java
@@ -2,7 +2,7 @@ package com.yapp.crew.domain.errors;
 
 public class SuspendedUserException extends RuntimeException {
 
-	public SuspendedUserException(String message) {
-		super(message);
+	public SuspendedUserException(Long userId) {
+		super("A suspended user tried to access with id: " + userId);
 	}
 }
