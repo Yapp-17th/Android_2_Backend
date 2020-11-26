f1b86fbad2f237a3cac78f6a222d0d091ea3e939
Alex Won <dnjscksdn98@gmail.com>
2020-11-26 23:27:24 +0900

    [feat] zuul post filter (#113)
    * chore: config logger
    
    * feat: add logger to base service
    
    * chore: log response body
    
    * chore: add application name logger field

--- base-service/src/main/java/com/yapp/crew/controller/ExerciseListController.java
+++ base-service/src/main/java/com/yapp/crew/controller/ExerciseListController.java
@@ -4,15 +4,18 @@ import com.yapp.crew.utils.ResponseDomain;
 import com.yapp.crew.dto.EnumListResponseDto;
 import com.yapp.crew.utils.EnumToList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j(topic = "Exercise List Controller")
 @RestController
 public class ExerciseListController {
 
 	@GetMapping(path = "/v1/exercise")
 	public ResponseEntity<?> getExerciseList() {
+		log.info("Get Exercise List");
 
 		List<String> exerciseList = EnumToList.exerciseEnumToList();
 		EnumListResponseDto enumListResponseDto = EnumListResponseDto.pass(ResponseDomain.EXERCISE.getName(), exerciseList);
