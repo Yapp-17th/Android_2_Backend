d364699c38afcbf9cd5b0edb0825d94b7fefb3a8
Alex Won <dnjscksdn98@gmail.com>
2020-11-26 22:21:53 +0900

    chore: config logger

--- base-service/src/main/java/com/yapp/crew/controller/AddressListController.java
+++ base-service/src/main/java/com/yapp/crew/controller/AddressListController.java
@@ -8,11 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class AddressListController {
 
 	@GetMapping(path = "/v1/address/city")
 	public ResponseEntity<?> getAddressCityList() {
+
 
 		List<String> addressList = EnumToList.addressEnumToList();
 		EnumListResponseDto enumListResponseDto = EnumListResponseDto.pass(ResponseDomain.ADDRESS_CITY.getName(), addressList);
