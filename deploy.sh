d726b30ba1d51c3939b627b89b67ff4651eb1ea4
Alex Won <dnjscksdn98@gmail.com>
2020-12-02 23:01:47 +0900

    deploy: setup v1.0.0-prod

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
-git pull
-sudo docker-compose down
-sudo docker image prune -f
-./gradlew clean build
-sudo docker-compose build --no-cache
-sudo docker-compose up -d
+if ["dev" == "$1"]; then
+  git pull
+  docker-compose down
+  docker image prune -f
+  ./gradlew clean build
+  docker-compose build --no-cache
+  docker-compose up -d
+
+elif ["prod" == "$1"]; then
+  git pull
+  docker-compose -f docker-compose.prod.yml down
+  docker image prune -f
+  ./gradlew clean build
+  docker-compose -f docker-compose.prod.yml build --no-cache
+  docker-compose -f docker-compose.prod.yml up -d
+
+else
+  echo "please send environment variable"
