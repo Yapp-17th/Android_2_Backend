9f04f75d6f8f1aa7d65b23edcb07db05b33827e3
soyeon Park <sypark9646@gmail.com>
2020-12-05 18:45:00 +0900

    style: board status show style (#161)

--- common-module/src/main/java/com/yapp/crew/domain/status/BoardStatus.java
+++ common-module/src/main/java/com/yapp/crew/domain/status/BoardStatus.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 @Getter
 public enum BoardStatus {
 
-	RECRUITING(0, "모집 중"),
-	COMPLETE(1, "모집 완료"),
-	FINISHED(2, "활동 종료"),
-	CANCELED(3, "모임 취소");
+	RECRUITING(0, "모집중"),
+	COMPLETE(1, "모집완료"),
+	FINISHED(2, "활동종료"),
+	CANCELED(3, "모임취소");
 
 	private final int code;
 	private final String name;
