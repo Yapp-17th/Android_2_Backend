9daa8baa637e59eeab629b8a571da88f2729e05c
soyeon <sypark9646@gmail.com>
2020-12-06 16:40:22 +0900

    add: init.sql, schema.sql -> 스키마에는 user suspended_day 필드 추가

--- init.sql
+++ init.sql
@@ -1,241 +1,48 @@
-DROP TABLE IF EXISTS `report`;
-DROP TABLE IF EXISTS `hidden_board`;
-DROP TABLE IF EXISTS `evaluation`;
-DROP TABLE IF EXISTS `message`;
-DROP TABLE IF EXISTS `chat_room`;
-DROP TABLE IF EXISTS `book_mark`;
-DROP TABLE IF EXISTS `applied_user`;
-DROP TABLE IF EXISTS `board`;
-DROP TABLE IF EXISTS `user_exercise`;
-DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `alarm`;
-DROP TABLE IF EXISTS `address`;
-DROP TABLE IF EXISTS `category`;
-DROP TABLE IF EXISTS `tag`;
+insert into address(id, city, created_at, updated_at) values(1, 'JONGNO_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(2, 'JUNG_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(3, 'YONGSAN_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(4, 'SEONGDONG_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(5, 'GWANGJIN_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(6, 'DONGDAEMUN_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(7, 'JUNGNANG_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(8, 'SEONGBUK_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(9, 'GANGBUK_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(10, 'DOBONG_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(11, 'NOWON_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(12, 'EUNPYEONG_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(13, 'SEODAEMUN_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(14, 'MAPO_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(15, 'YANGCHEON_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(16, 'GANGSEO_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(17, 'GURO_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(18, 'GEUMCHEON_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(19, 'YEONGDEUNGPO_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(20, 'DONGJAK_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(21, 'GWANAK_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(22, 'SEOCHO_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(23, 'GANGNAM_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(24, 'SONGPA_GU', now(), now());
+insert into address(id, city, created_at, updated_at) values(25, 'GANGDONG_GU', now(), now());
 
-CREATE TABLE `tag` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
+insert into category(id, exercise, created_at, updated_at) values(1, 'SOCCER', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(2, 'BASKETBALL', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(3, 'TENNIS', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(4, 'RUNNING', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(5, 'HIKING', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(6, 'BICYCLE', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(7, 'CLIMBING', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(8, 'VOLLYBALL', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(9, 'BASEBALL', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(10, 'FUTSAL', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(11, 'BADMINTON', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(12, 'RUGBY', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(13, 'BOXING', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(14, 'ICE_HOKEY', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(15, 'GOLF', now(), now());
+insert into category(id, exercise, created_at, updated_at) values(16, 'OTHERS', now(), now());
 
-CREATE TABLE `category` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `exercise` varchar(255) NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
+insert into tag(id, name, created_at, updated_at) values(1, 'TIGHT_USER', now(), now());
+insert into tag(id, name, created_at, updated_at) values(2, 'FUN_USER', now(), now());
 
-CREATE TABLE `address` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `city` varchar(255) NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `alarm` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) NOT NULL,
-  `type` int NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `access_token` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `intro` varchar(255) NOT NULL,
-  `nickname` varchar(100) NOT NULL,
-  `oauth_id` varchar(100) NOT NULL,
-  `status` varchar(255) NOT NULL DEFAULT 'ACTIVE',
-  `username` varchar(100) NOT NULL,
-  `address_id` bigint NOT NULL,
-  `suspended_day` int NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `nickname` (`nickname`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `access_token` (`access_token`),
-  UNIQUE KEY `oauth_id` (`oauth_id`),
-  KEY `FK_User_addressId` (`address_id`),
-  CONSTRAINT `FK_User_addressId` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `user_exercise` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `category_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_UserExercise_categoryId` (`category_id`),
-  KEY `FK_UserExercise_userId` (`user_id`),
-  CONSTRAINT `FK_UserExercise_categoryId` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
-  CONSTRAINT `FK_UserExercise_userId` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `board` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) NOT NULL,
-  `place` varchar(255) NOT NULL,
-  `recruit_count` int NOT NULL,
-  `status` int NOT NULL DEFAULT 0,
-  `title` varchar(255) NOT NULL,
-  `address_id` bigint NOT NULL,
-  `category_id` bigint NOT NULL,
-  `tag_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `starts_at` datetime(6) NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_Board_addressId` (`address_id`),
-  KEY `FK_Board_categoryId` (`category_id`),
-  KEY `FK_Board_tagId` (`tag_id`),
-  KEY `FK_Board_userId` (`user_id`),
-  CONSTRAINT `FK_Board_categoryId` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
-  CONSTRAINT `FK_Board_addressId` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`),
-  CONSTRAINT `FK_Board_userId` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_Board_tagId` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `applied_user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `status` varchar(255) NOT NULL DEFAULT 'PENDING',
-  `board_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_AppliedUser_boardId` (`board_id`),
-  KEY `FK_AppliedUser_userId` (`user_id`),
-  CONSTRAINT `FK_AppliedUser_userId` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_AppliedUser_boardId` FOREIGN KEY (`board_id`) REFERENCES `board` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `book_mark` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `board_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_BookMark_boardId` (`board_id`),
-  KEY `FK_BookMark_userId` (`user_id`),
-  CONSTRAINT `FK_BookMark_boardId` FOREIGN KEY (`board_id`) REFERENCES `board` (`id`),
-  CONSTRAINT `FK_BookMark_userId` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `chat_room` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `status` varchar(255) NOT NULL DEFAULT 'ACTIVE',
-  `board_id` bigint NOT NULL,
-  `guest_id` bigint NOT NULL,
-  `host_id` bigint NOT NULL,
-  `guest_exited` boolean NOT NULL DEFAULT FALSE,
-  `host_exited` boolean NOT NULL DEFAULT FALSE,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_ChatRoom_boardId` (`board_id`),
-  KEY `FK_ChatRoom_guestId` (`guest_id`),
-  KEY `FK_ChatRoom_hostId` (`host_id`),
-  CONSTRAINT `FK_ChatRoom_guestId` FOREIGN KEY (`guest_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_ChatRoom_hostId` FOREIGN KEY (`host_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_ChatRoom_boardId` FOREIGN KEY (`board_id`) REFERENCES `board` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `message` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `content` varchar(500) NOT NULL,
-  `is_guest_read` boolean NOT NULL DEFAULT FALSE,
-  `is_host_read` boolean NOT NULL DEFAULT FALSE,
-  `type` varchar(255) NOT NULL,
-  `chat_room_id` bigint NOT NULL,
-  `sender_id` bigint NOT NULL,
-  `message_id` int NOT NULL DEFAULT '0',
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_Message_chatRoomId` (`chat_room_id`),
-  KEY `FK_Message_senderId` (`sender_id`),
-  CONSTRAINT `FK_Message_chatRoomId` FOREIGN KEY (`chat_room_id`) REFERENCES `chat_room` (`id`),
-  CONSTRAINT `FK_Message_senderId` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `evaluation` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `evaluate_id` bigint NOT NULL,
-  `evaluated_id` bigint NOT NULL,
-  `is_dislike` boolean NOT NULL DEFAULT FALSE,
-  `is_like` boolean NOT NULL DEFAULT FALSE,
-  `board_id` bigint NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_Evaluation_boardId` (`board_id`),
-  CONSTRAINT `FK_Evaluation_boardId` FOREIGN KEY (`board_id`) REFERENCES `board` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `hidden_board` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `board_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_HiddenBoard_boardId` (`board_id`),
-  KEY `FK_HiddenBoard_userId` (`user_id`),
-  CONSTRAINT `FK_HiddenBoard_userId` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_HiddenBoard_boardId` FOREIGN KEY (`board_id`) REFERENCES `board` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `report` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) NULL,
-  `type` int NOT NULL,
-  `reported_id` bigint NOT NULL,
-  `reporter_id` bigint NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `FK_Report_reportedId` (`reported_id`),
-  KEY `FK_Report_reporterId` (`reporter_id`),
-  CONSTRAINT `FK_Report_reportedId` FOREIGN KEY (`reported_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_Report_reporterId` FOREIGN KEY (`reporter_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB
-  DEFAULT CHARSET=utf8mb4
-  COLLATE=utf8mb4_0900_ai_ci;
+insert into user(id, access_token, oauth_id, username, nickname, email, intro, status, address_id, created_at, updated_at) values(-9, "explanet_bot", "explanet_bot", "운동플래닛", "운동플래닛", "ex_planet@gmail.com", "운동플래닛", "ACTIVE", 1, now(), now());
+insert into user(id, access_token, oauth_id, username, nickname, email, intro, status, address_id, created_at, updated_at) values(-10, "forbidden_user", "forbidden_user", "알수없음", "알수없음", "forbidden_user@gmail.com", "알수없음", "ACTIVE", 1, now(), now());
