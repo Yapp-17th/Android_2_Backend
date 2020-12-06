4fb9bdda0b1ca9deafe07d1da1775311c52e7af7
Alex Won <dnjscksdn98@gmail.com>
2020-12-06 17:49:12 +0900

    fix: register custom handshake interceptor (#173)

--- chatting-service/src/main/java/com/yapp/crew/config/WebSocketConfig.java
+++ chatting-service/src/main/java/com/yapp/crew/config/WebSocketConfig.java
@@ -1,6 +1,10 @@
 package com.yapp.crew.config;
 
+import com.yapp.crew.auth.Auth;
+import com.yapp.crew.domain.repository.UserRepository;
+import com.yapp.crew.websocket.CustomHandshakeInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -12,10 +16,21 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	private final Auth auth;
+
+	private final UserRepository userRepository;
+
+	@Autowired
+	public WebSocketConfig(Auth auth, UserRepository userRepository) {
+		this.auth = auth;
+		this.userRepository = userRepository;
+	}
+
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry
 				.addEndpoint("/ws/chat")
+				.addInterceptors(new CustomHandshakeInterceptor(auth, userRepository))
 				.setAllowedOrigins("*")
 				.withSockJS();
 	}
