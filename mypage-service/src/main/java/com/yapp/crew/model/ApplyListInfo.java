443efb1aafefbd0c5cb65576a6393262f621c090
soyeon Park <sypark9646@gmail.com>
2020-12-07 22:47:26 +0900

    refactor: user status에 따라 리턴 타입 다르게 (#183)
    * refactor: user status에 따라 리턴 타입 다르게
    
    * refactor: valid user 체크 함수로 대체

--- mypage-service/src/main/java/com/yapp/crew/model/ApplyListInfo.java
+++ mypage-service/src/main/java/com/yapp/crew/model/ApplyListInfo.java
@@ -3,9 +3,13 @@ package com.yapp.crew.model;
 import com.yapp.crew.domain.model.Board;
 import com.yapp.crew.domain.model.ChatRoom;
 import com.yapp.crew.domain.model.User;
+import com.yapp.crew.domain.status.AppliedStatus;
+import com.yapp.crew.domain.status.UserStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor
 public class ApplyListInfo {
@@ -18,17 +22,88 @@ public class ApplyListInfo {
 	private Long chattingRoomId;
 	private Long boardId;
 
-	public static ApplyListInfo build(long hostId, User guest, Board board, ChatRoom chatRoom) {
-		ApplyListInfo applyListInfo = new ApplyListInfo();
-		applyListInfo.hostId = hostId;
-		applyListInfo.guestId = guest.getId();
-		applyListInfo.guestName = guest.getNickname();
-		applyListInfo.applyStatus = ApplyStatusInfo.build(board.getAppliedUsers().stream()
-				.filter(appliedUser -> appliedUser.getUser().getId().equals(guest.getId()))
-				.findFirst().get().getStatus());
-		applyListInfo.chattingRoomId = chatRoom != null ? chatRoom.getId() : 0L;
-		applyListInfo.boardId = board.getId();
+	public static ApplyListInfoBuilder getBuilder() {
+		return new ApplyListInfoBuilder();
+	}
 
-		return applyListInfo;
+	public static class ApplyListInfoBuilder {
+
+		private Long hostId = -1L;
+		private Long guestId = -1L;
+		private String guestName = "(알수없음)";
+		private Boolean isHost = false;
+		private ApplyStatusInfo applyStatus = ApplyStatusInfo.build(AppliedStatus.PENDING);
+		private Long chattingRoomId = -1L;
+		private Long boardId = -1L;
+
+		public ApplyListInfoBuilder withHostId(Long hostId) {
+			this.hostId = hostId;
+			return this;
+		}
+
+		public ApplyListInfoBuilder withGuestId(Long guestId) {
+			this.guestId = guestId;
+			return this;
+		}
+
+		public ApplyListInfoBuilder withGuestName(String guestName) {
+			this.guestName = guestName;
+			return this;
+		}
+
+		public ApplyListInfoBuilder withIsHost(Boolean isHost) {
+			this.isHost = isHost;
+			return this;
+		}
+
+		public ApplyListInfoBuilder withApplyStatusInfo(AppliedStatus appliedStatus) {
+			this.applyStatus = ApplyStatusInfo.build(appliedStatus);
+			return this;
+		}
+
+		public ApplyListInfoBuilder withChattingRoomId(Long chattingRoomId) {
+			this.chattingRoomId = chattingRoomId;
+			return this;
+		}
+
+		public ApplyListInfoBuilder withBoardId(Long boardId) {
+			this.boardId = boardId;
+			return this;
+		}
+
+		public ApplyListInfo build() {
+			ApplyListInfo applyListInfo = new ApplyListInfo();
+			applyListInfo.setHostId(hostId);
+			applyListInfo.setGuestId(guestId);
+			applyListInfo.setGuestName(guestName);
+			applyListInfo.setIsHost(isHost);
+			applyListInfo.setApplyStatus(applyStatus);
+			applyListInfo.setChattingRoomId(chattingRoomId);
+			applyListInfo.setBoardId(boardId);
+
+			return applyListInfo;
+		}
+
+	}
+
+	public static ApplyListInfo build(long hostId, User guest, Board board, ChatRoom chatRoom) {
+		Long chattingRoomId = chatRoom != null ? chatRoom.getId() : 0L;
+
+		ApplyListInfoBuilder applyListInfoBuilder = ApplyListInfo.getBuilder()
+				.withHostId(hostId)
+				.withApplyStatusInfo(board.getAppliedUsers().stream()
+						.filter(appliedUser -> appliedUser.getUser().getId().equals(guest.getId()))
+						.findFirst().get().getStatus())
+				.withChattingRoomId(chattingRoomId)
+				.withBoardId(board.getId());
+
+		if (guest.isValidUser()) {
+			return applyListInfoBuilder
+					.withGuestId(guest.getId())
+					.withGuestName(guest.getNickname())
+					.build();
+		}
+
+		return applyListInfoBuilder.build();
 	}
 }
