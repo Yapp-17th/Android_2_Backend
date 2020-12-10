5d61d3c60bb1e77b5548fe5b88c34199da1ca585
Alex Won <dnjscksdn98@gmail.com>
2020-12-10 21:07:16 +0900

    fix: remove volumes at deploy (#202)

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ then
   git pull
   docker-compose down
   docker image prune -f
+  docker volume prune -f
   ./gradlew clean bootJar
   docker-compose build --no-cache
   docker-compose up -d
@@ -14,6 +15,7 @@ then
   git pull
   docker-compose -f docker-compose.prod.yml down
   docker image prune -f
+  docker volume prune -f
   ./gradlew clean bootJar
   docker-compose -f docker-compose.prod.yml build --no-cache
   docker-compose -f docker-compose.prod.yml up -d
